decd21d36d930f9584923f434d15a45a0cdb8cc3
EmmanuelTejedaRomero <86384454+EmmanuelTejedaRomero@users.noreply.github.com>
2021-06-24 14:27:24 -0500

    Delete DemoblazeAutomation directory

--- DemoblazeAutomation/src/test/java/myRunner/TestExecution.java
+++
@@ -1,9 +0,0 @@
-package myRunner;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features ="src/test/java/features/StepsByDemoBlze.feature", glue = "stepsDefinition")
-public class TestExecution extends AbstractTestNGCucumberTests{
-
-}
